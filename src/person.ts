c05b3d276447a50228e88cf74813acbbc9456223
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-10-07 15:47:52 -0300

    feat: add bad code

--- src/person.ts
+++ src/person.ts
@@ -1,17 +1,43 @@
-export default class Person {
-  eat (): void {
-    console.log('eat method')
+export default class Calculator {
+  private result: number
+
+  private constructor () {
+    this.result = 0
   }
 
-  run (): void {
-    console.log('run method')
+  public add (value: number): Calculator {
+    this.result += value
+    return this
   }
 
-  static build (): Person {
-    let text = 'text'
-    let text2 = 33213213123123123213213213213321312312 + 'ba'
-    console.log(text)
-
-    return new Person()
+  public subtract (value: number): Calculator {
+    this.result -= value
+    return this
   }
+
+  public addDuplicated0(value: number): Calculator {
+    this.result += value
+    return this
+  }
+
+  public addDuplicated1(value: number): Calculator {
+    this.result += value
+    return this
+  }
+
+  public multiply (value: number): Calculator {
+    this.result *= value
+    return this
+  }
+
+  // @ts-ignore
+  public getResult (): number {
+    console.log(this.result)
+  }
+
+  static build (): Calculator {
+    return new Calculator()
+  }
+
+
 }
